2a34fa9ea3512f8c87e38fe581d0a17dd1076122
Eugene-301 <ablepiha@duck.com>
2025-04-07 01:02:49 +0300

    Final meteors spawn logic

--- scripts/meteors.js
+++ scripts/meteors.js
@@ -1,12 +1,16 @@
 const root = document.querySelector(".root");
+const maxPosition = 100 - meteorSize;
 
 export const initMeteors = () => {
   setInterval(() => {
-    const radomPosition = Math.random() * 100;
-    const meteorHTML = `<div class='meteor' style="left: ${radomPosition}%"><img src='./assets/images/enemy-meteor.png'alt='вражеский метеорит'class='meteor__img img'/></div>`;
+    let randomPosition = Math.floor(Math.random() * (maxPosition + 1));
 
-    root.insertAdjacentHTML("afterbegin", meteorHTML);
-  }, 3000);
+    const meteorHTML = `<div class='meteor' style="left: ${randomPosition}%"><img src='./assets/images/enemy-meteor.png'alt='вражеский метеорит'class='meteor__img img'/></div>`;
+
+    let colya = root.insertAdjacentHTML("afterbegin", meteorHTML);
+
+    console.log(colya);
+  }, 100);
 };
 
 export default {
