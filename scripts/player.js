5fcf1a91765dbf4988bfaf58922059f339e1346c
Eugene-301 <ablepiha@duck.com>
2025-04-07 23:23:50 +0300

    Improved player controls

--- scripts/player.js
+++ scripts/player.js
@@ -7,20 +7,42 @@ const playerSize = (100 * player.offsetWidth) / window.innerWidth;
 
 let position = 0;
 
+let keysState = {
+  left: false,
+  right: false,
+};
+
+const checkIsMove = setInterval(() => {
+  if (keysState.right) {
+    if (position >= positionMax - playerSize) return;
+    position += 2;
+    player.style.left = `${position}%`;
+  } else if (keysState.left) {
+    if (position === positionMin) return;
+
+    position -= 2;
+    player.style.left = `${position}%`;
+  }
+}, 75);
+
 export const initPlayer = () => {
   document.addEventListener("keydown", (e) => {
     const keyPressed = e.key;
 
     if (keyPressed === "ArrowRight" || keyPressed === "d") {
-      if (position >= positionMax - playerSize) return;
-
-      position += 2;
-      player.style.left = `${position}%`;
+      keysState.right = true;
     } else if (keyPressed === "ArrowLeft" || keyPressed === "a") {
-      if (position === positionMin) return;
+      keysState.left = true;
+    }
+  });
 
-      position -= 2;
-      player.style.left = `${position}%`;
+  document.addEventListener("keyup", (e) => {
+    const keyPressed = e.key;
+
+    if (keyPressed === "ArrowRight" || keyPressed === "d") {
+      keysState.right = false;
+    } else if (keyPressed === "ArrowLeft" || keyPressed === "a") {
+      keysState.left = false;
     }
   });
 };
